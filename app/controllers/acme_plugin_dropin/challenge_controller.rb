982ecd62808e9c98bb1edbd8c776471fe8d82d1e
Francois Harbec <fharbec@gmail.com>
2020-02-27 11:00:13 -0800

    oops

--- app/controllers/acme_plugin_dropin/challenge_controller.rb
+++ app/controllers/acme_plugin_dropin/challenge_controller.rb
@@ -1,5 +1,5 @@
 module AcmePluginDropin
-  class ChallengeControlller < ApplicationController
+  class ChallengeController < ApplicationController
     def index
       if params[:challenge].present? && params[:challenge].length >= 16 && params[:challenge].length <= 256
         render plain: LetsEncrypt.current_challenge, status: :ok
